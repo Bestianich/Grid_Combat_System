e29a64e2672c18efae07a775415be48a593bb77c
Davide Fedeli <137522657+Bestianich@users.noreply.github.com>
2025-07-28 18:38:39 +0200

    feat: custom mesh for grid

--- src/Grid_Combat_System/Assets/Scripts/GridGenerator.cs
+++ src/Grid_Combat_System/Assets/Scripts/GridGenerator.cs
@@ -1,10 +1,16 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
+[ExecuteInEditMode]
 public class GridGenerator : MonoBehaviour
 {
-    [SerializeField] private GameObject _plane;
+
+    [SerializeField] private int _sizeX = 100;
+    [SerializeField] private int _sizeY = 50;
+    [SerializeField] private float _tileSize = 2f;
+
     [SerializeField] private GameObject _cellPrefab;
 
     private List<Vector3> _points = null;
@@ -12,20 +18,75 @@ public class GridGenerator : MonoBehaviour
     private void Start()
     {
         _points = new List<Vector3>();
-        _points = new List<Vector3>(_plane.GetComponent<MeshFilter>().mesh.vertices);
+        BuildMesh();
+    }
+
+    public void BuildMesh()
+    {
+        int numTiles = _sizeX * _sizeY;
+        int numTriangles = numTiles * 2;
+
+        int vertX = _sizeX + 1;
+        int vertY = _sizeY + 1;
+
+        int numVertices = vertX * vertY;
+
+        Vector3[] vertices = new Vector3[numVertices];
+        int[] triangles = new int[numTriangles * 3];
+        Vector3[] normals = new Vector3[numVertices];
+        Vector2[] uv = new Vector2[numVertices];
+
+        for (int z = 0; z < vertY; z++) {
+            for (int x = 0; x < vertX; x++) {
+                vertices[ z * vertX + x] = new Vector3(z * _tileSize, 0, x * _tileSize);
+                normals[z * vertX + x]  = Vector3.up;
+                uv[ z * vertX + x] = new Vector2((float)z / (float)_sizeX, (float)x / (float)_sizeY);
+            }
+        }
+
+        for (int z = 0; z < _sizeY; z++) {
+            for (int x = 0; x < _sizeX; x++) {
+                int squareIndex = z * _sizeX + x;
+                int triangleOffset = squareIndex * 6;
+                triangles[triangleOffset + 0] = z * vertX + x + 0;
+                triangles[triangleOffset + 1] = z * vertX + x + vertX +  1;
+                triangles[triangleOffset + 2] = z * vertX + x + vertX +  0;
+                triangles[triangleOffset + 3] = z * vertX + x +  0;
+                triangles[triangleOffset + 4] = z * vertX + x +  1;
+                triangles[triangleOffset + 5] = z * vertX + x + vertX + 1;
+            }
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.normals = normals;
+        mesh.uv = uv;
+
+        MeshFilter mf = GetComponent<MeshFilter>();
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        MeshCollider mc = GetComponent<MeshCollider>();
+        mf.mesh = mesh;
+
+        Debug.Log(mf.mesh.vertices.Length);
+
+        _points = new List<Vector3>();
+        _points = new List<Vector3>(GetComponent<MeshFilter>().mesh.vertices);
+        Debug.Log(GetComponent<MeshFilter>().mesh);
+        Debug.Log(_points.Count);
         foreach (Vector3 point in _points)
         {
             Instantiate(_cellPrefab, point + new Vector3(0f , 0.01f , 0f) , _cellPrefab.transform.rotation , this.transform);
         }
-    }
 
+    }
     private void OnDrawGizmos()
     {
         if (_points == null)
             return;
         foreach (Vector3 point in _points)
         {
-            Gizmos.DrawCube(point, Vector3.one);
+            Gizmos.DrawSphere(point, 0.2f);
         }
     }
 }
